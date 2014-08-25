7feb6926a470a7cc153b20d5e36ff99d9b88b884
David Dolinar <synthdave@gmail.com>
2014-08-24 21:42:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DomoApp::Application.config.secret_token = '514474bf42b23973f5c567cfe9a66a4d5b62398d60aca08658d2f104edfac1aad011aa5fbcc14acad77118f7568043dd259905a1ac1c1bb28bdfcc07254df536'
